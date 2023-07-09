e7615a2646b20293f10a983a0de4eb5849632868
unknownpersonog <drnehapawar98@gmail.com>
2023-07-09 15:06:02 +0530

    Made Database load before listening

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
-mongoose
-    .connect(`${process.env.MONGODB_DATABASE_URI}`)
-    .then(() => console.log('Connected to Databased.'))
-    .catch((err) => console.log('Error occured while connecting: ' + err))
+export async function connectDb() {
+    await mongoose
+        .connect(`${process.env.MONGODB_DATABASE_URI}`)
+        .then(() => console.log('Connected to Databased.'))
+        .catch((err) => console.log('Error occured while connecting: ' + err))
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { config } from 'dotenv'; 
 import { createApp } from './utils/createApp';
 config();
-import './database'
-
+import { connectDb } from './database'
 
 const PORT = process.env.PORT || 3001;
 
 async function main() {
     console.log(`Running in ${process.env.ENVIRONMENT} mode.`)
     try {
+        await connectDb();
         const app = createApp();
         app.listen(PORT, () => console.log(`Running on Port ${process.env.PORT}`));
     } catch (err) {
