6472cafa9acec667706439a43783df0917d6f0ca
unknownpersonog <drnehapawar98@gmail.com>
2023-07-10 21:22:43 +0530

    - Added dist back to gitignore - Updated email design - Removed dist from source code

--- dist/utils/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DISCORD_API_URL = void 0;
-exports.DISCORD_API_URL = 'https://discord.com/api/v10';
--- src/routes/users/verify/index.ts
+++ src/routes/users/verify/index.ts
@@ -43,8 +43,58 @@ router.post('/mail', async(req: Request, res: Response) => {
       subject: 'Email Verification',
       text: `Your code for verification is: 
              ${verificationToken}`,
-      html: `<p>Your code for verification is:</p>
-             <p><h1>${verificationToken}</h1></p>`,
+      html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Verify your login</title>
+  <!--[if mso]><style type="text/css">body, table, td, a { font-family: Arial, Helvetica, sans-serif !important; }</style><![endif]-->
+</head>
+
+<body style="font-family: Helvetica, Arial, sans-serif; margin: 0px; padding: 0px; background-color: #ffffff;">
+  <table role="presentation" data-darkreader-inline-border-top="" data-darkreader-inline-border-right="" data-darkreader-inline-border-bottom=""
+    data-darkreader-inline-border-left=""
+    style="width: 100%; border-collapse: collapse; border: 0px; border-spacing: 0px; font-family: Arial, Helvetica, sans-serif; --darkreader-inline-border-top: initial; --darkreader-inline-border-right: initial; --darkreader-inline-border-bottom: initial; --darkreader-inline-border-left: initial; background-color: rgb(239, 239, 239); --darkreader-inline-bgcolor: #212425;"
+    data-darkreader-inline-bgcolor="">
+    <tbody>
+      <tr>
+        <td align="center" style="padding: 1rem 2rem; vertical-align: top; width: 100%;">
+          <table role="presentation" data-darkreader-inline-border-top="" data-darkreader-inline-border-right=""
+            data-darkreader-inline-border-bottom="" data-darkreader-inline-border-left=""
+            style="max-width: 600px; border-collapse: collapse; border: 0px; border-spacing: 0px; text-align: left; --darkreader-inline-border-top: initial; --darkreader-inline-border-right: initial; --darkreader-inline-border-bottom: initial; --darkreader-inline-border-left: initial;">
+            <tbody>
+              <tr>
+                <td style="padding: 40px 0px 0px;">
+                  <div style="text-align: left;">
+                    <div style="padding-bottom: 20px;" align="center"><img src="https://cdn.jsdelivr.net/gh/unknownpersonog/unknownvps-client@assets/png/server.png" alt="Company" style="width: 56px;"></div>
+                  </div>
+                  <div style="padding: 20px; background-color: rgb(255, 255, 255); --darkreader-inline-bgcolor: #181a1b;"
+                    data-darkreader-inline-bgcolor="">
+                    <div style="color: rgb(0, 0, 0); text-align: left; --darkreader-inline-color: #e8e6e3;" data-darkreader-inline-color="">
+                      <h1 style="margin: 1rem 0;" align="center">Verification code</h1>
+                      <p style="padding-bottom: 16px;" align="center">Please use the verification code below to sign in.</p>
+                      <p style="padding-bottom: 16px; font-size: 30px; " align="center"><strong style="font-size: 130%">${verificationToken}</strong></p>
+                      <p style="padding-bottom: 16px;" align="center">If you didn’t request this, you can ignore this email.</p>
+                      <p style="padding-bottom: 16px" align="center">Thanks,<br> ${process.env.COMPANY_NAME}</p>
+                    </div>
+                  </div>
+                  <div style="padding-top: 20px; color: rgb(153, 153, 153); text-align: center; --darkreader-inline-color: #a8a095;"
+                    data-darkreader-inline-color="">
+                    <p style="padding-bottom: 16px">By UnknownVPS Ltd.</p>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+
+</html>`,
     };
 
     transporter.sendMail(mailOptions);
