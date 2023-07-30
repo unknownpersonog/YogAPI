7927b9f5a0ed4e0a8b6e204103e53abbe4c689a0
unknownpersonog <drnehapawar98@gmail.com>
2023-07-30 17:13:02 +0530

    Ping now returns time taken to process using headers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,23 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
+import responseTime from 'response-time';
 import usersRouter from './users';
 
 const router = Router();
 
 router.use('/users', usersRouter);
 
-router.use('/ping', async (req: Request, res: Response) => {
+router.use('/ping', responseTime())
+
+router.get('/ping', async (req: Request, res: Response, next: NextFunction) => {
     try {
-        res.status(200).send('Pong!');
-        return;
+      const responseObj = {
+        message: 'Pong!',
+      };
+  
+      res.json(responseObj);
+    } catch (err) {
+      res.status(501).send('Internal Server Error');
     }
-    catch (err) {
-        res.status(501).send('Internal Server Error')
-    }
-});
+  });
+
 export default router;
