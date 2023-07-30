61f70a60529a4419988189500bed55e2373ed54a
unknownpersonog <drnehapawar98@gmail.com>
2023-07-30 15:53:26 +0530

    Ping command to get API Status

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,16 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import usersRouter from './users';
 
 const router = Router();
 
 router.use('/users', usersRouter);
 
+router.use('/ping', (res: Response) => {
+    try {
+        res.status(200).send('Pong!');
+    }
+    catch (err) {
+        res.status(501).send('Internal Server Error')
+    }
+});
 export default router;
