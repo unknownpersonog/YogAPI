478483ebe7e589e9edd7ea500aa66067644fb2bd
unknownpersonog <drnehapawar98@gmail.com>
2023-08-01 18:15:01 +0530

    Added /assignvps

--- src/database/schemas/vps.ts
+++ src/database/schemas/vps.ts
@@ -4,7 +4,8 @@ const vpsSchema = new mongoose.Schema({
     id: { type: Number, required: true, unique: true },
     name: { type: String, required: true },
     os: { type: String, required: true },
-    port: { type: Number, required: false, unique: true },
+    port: { type: Number, required: true, unique: true },
+    owner: { type: String, default: "N/A", required: false },
 });
   
   export default mongoose.model('vpsdata', vpsSchema)
