c52cb0ab453ddf3040009caa62c16d64634e81ad
unknownpersonog <drnehapawar98@gmail.com>
2023-10-18 18:50:05 +0530

    error

--- src/services/settingsParser.ts
+++ src/services/settingsParser.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import { Settings } from '../utils/types';
 
 export function settingsParser() {
-const rawSettingsData = fs.readFileSync('../../settings.json', 'utf8');
+const rawSettingsData = fs.readFileSync('settings.json', 'utf8');
 const settingsData: { settings: Settings } = JSON.parse(rawSettingsData);
 
 return settingsData.settings;
